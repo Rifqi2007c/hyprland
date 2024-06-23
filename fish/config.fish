9b5cb9d6fbfd59575a0bd8a21298745e8611e5c5
Rifqi2007c <158813804+Rifqi2007c@users.noreply.github.com>
2024-06-23 19:51:00 +0800

    Delete fish directory

--- fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    starship init fish | source
-    sttt scanline --scanline-reverse true -d 0.8
-    set -g fish_greeting
-end
--- fish/functions/fish_greeting.fish
+++
@@ -1,5 +0,0 @@
-function fish_greeting
-
-    echo "Hello.user(fish);"
-
-end
