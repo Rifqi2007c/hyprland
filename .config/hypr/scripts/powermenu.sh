717039cdf152a920538b7f0b95be241630ab49e3
Rifqi2007c <158813804+Rifqi2007c@users.noreply.github.com>
2026-01-08 01:50:17 +0800

    Add files via upload

---
+++ .config/hypr/scripts/powermenu.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+rofi_cmd() {
+  rofi -dmenu \
+    -theme ~/.config/hypr/rofi/powermenu.rasi -x11
+}
+
+chosen=$(printf "      \n⏻Shutdown\n󰜉Reboot\n󰗼Logout\n" | rofi_cmd)
+
+case "$chosen" in
+
+"⏻Shutdown") poweroff ;;
+"󰜉Reboot") reboot ;;
+"󰗼Logout") hyprctl dispatch exit ;;
+*) exit 1 ;;
+
+esac
