ccae1ad2cb7b5dcdf05bcb0acc91a093bff586c6
Rifqi2007c <158813804+Rifqi2007c@users.noreply.github.com>
2026-04-15 22:40:09 +0800

    Update wallselect.sh

--- .config/hypr/scripts/wallselect.sh
+++ .config/hypr/scripts/wallselect.sh
@@ -23,14 +23,14 @@ wall_selection=$(find "${wall_dir}" -type f \( -iname "*.jpg" -o -iname "*.jpeg"
 [[ -n "$wall_selection" ]] || exit 1
 killall cava
 killall waybar
-swww img "$HOME/.config/hypr/wallpapers/loading_smoothed_nes.gif" --transition-type outer --transition-duration 1 --transition-fps 60 --transition-step 20 && sleep 2.6
-swww img "${wall_dir}"/"${wall_selection}" --transition-type center --transition-duration 1 --transition-fps 60 --transition-step 20 && sleep 0.7
+awww img "$HOME/.config/hypr/wallpapers/loading_smoothed_nes.gif" --transition-type outer --transition-duration 1 --transition-fps 60 --transition-step 20 && sleep 2.6
+awww img "${wall_dir}"/"${wall_selection}" --transition-type center --transition-duration 1 --transition-fps 60 --transition-step 20 && sleep 0.7
 wal -i "${wall_dir}"/"${wall_selection}" --contrast 4 --saturate 0 --cols16 darken
 
 # reload swaync css
 swaync-client -rs
 # change obsidian color
-bash ~/.config/hypr/scripts/pywal-obsidianmd/pywal-obsidianmd.sh "/home/rifqi/gesellschaft/"
+#bash ~/.config/hypr/scripts/pywal-obsidianmd/pywal-obsidianmd.sh "/home/rifqi/gesellschaft/"
 
 # start waybar
 waybar &
@@ -40,7 +40,7 @@ waybar &
 #waybar -c ~/.config/waybar/config-secondary &
 
 ## gtk app
-gsettings set org.gnome.desktop.interface gtk-theme 'catppuccin-macchiato-blue-standard+default' && gsettings set org.gnome.desktop.interface gtk-theme 'PywalTheme'
+#gsettings set org.gnome.desktop.interface gtk-theme 'catppuccin-macchiato-blue-standard+default' && gsettings set org.gnome.desktop.interface gtk-theme 'PywalTheme'
 
 ## firefox userContent reload
 bash ~/.config/hypr/scripts/firefox-userContent.sh
